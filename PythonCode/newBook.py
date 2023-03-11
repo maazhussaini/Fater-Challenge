8cfeac042827ade7fdd5c89164caa302bdb7ce4e
Syed Maaz Hussaini <skr2570@gmail.com>
2023-03-11 23:46:36 +0100

    active_user_complete

--- PythonCode/newBook.py
+++ PythonCode/newBook.py
@@ -78,7 +78,7 @@ def detect_outliers(registry_game):
     plt.show()
 
 
-def merge_all_data(registry_game, product_loaded, product_ean_conversion, missions_players, app_accesses, awards_moms):
+def tier_based_product(product_loaded, product_ean_conversion, missions_players):
     # Merge relevant columns from different tables using the id_player as key
     product_mission_merge = pd.merge(missions_players, product_loaded,
                                      how='inner', on='id_player')
@@ -137,14 +137,24 @@ def feature_engineering(registry_game):
 
 def active_users(missions_players, app_accesses):
 
-    # Get today's date
-    todayDate = datetime.today().strftime('%Y-%m-%d')
+    app_accesses['updated_at'] = pd.to_datetime(
+        app_accesses['updated_at'])
 
-    # Convert today's date to datetime
-    todayDate = pd.DataFrame(
-        [pd.to_datetime(todayDate)], columns=['TodayDate'])
+    # extract the date part and save it to a new column 'date_only'
+    app_accesses['updated_at'] = app_accesses['updated_at'].dt.date
 
-    todayDate['TodayDate'] = pd.to_datetime(todayDate['TodayDate']).dt.date
+    app_accesses_dates = app_accesses.groupby('id_player')[
+        'updated_at'].max()
+
+    app_accesses_dates = app_accesses_dates.rename_axis(
+        'id_player').reset_index()
+
+    """
+    Assuming max date is a today date.
+    """
+    todayDate_access = max(app_accesses_dates['updated_at'].to_list())
+
+    # ---------------------------------------------------------------------------------------
 
     missions_players['created_at'] = pd.to_datetime(
         missions_players['created_at'])
@@ -155,42 +165,67 @@ def active_users(missions_players, app_accesses):
     missions_players_dates = missions_players.groupby('id_player')[
         'created_at'].max()
 
-    missions_players_dates_max = missions_players_dates.index
+    missions_players_dates = missions_players_dates.rename_axis(
+        'id_player').reset_index()
 
-    print(missions_players_dates.head())
-    print(missions_players_dates.info())
+    """
+    Assuming max date is a today date.
+    """
+    todayDate_mission = max(missions_players_dates['created_at'].to_list())
 
-    # date_result = todayDate['TodayDate'] - missions_players_dates['created_at']
+    todayDate = max(todayDate_mission, todayDate_access)
+    print(todayDate)
 
-    # missions_players_dates['diff_date'] = (
-    #     todayDate['TodayDate'] - missions_players_dates['created_at']).dt.days
+    # ---------------------------------------------------------------------------------------
 
-    # print(missions_players_dates)
+    app_accesses_dates['todayDate_access'] = todayDate
+    app_accesses_dates['diff_date_access'] = (app_accesses_dates['todayDate_access'] -
+                                              app_accesses_dates['updated_at']).dt.days
+
+    print("\n\napp_accesses_dates: \n",
+          app_accesses_dates[app_accesses_dates['diff_date_access'] > 30].shape)
+
+    missions_players_dates['todayDate_mission'] = todayDate
+    missions_players_dates['diff_date_mission'] = (missions_players_dates['todayDate_mission'] -
+                                                   missions_players_dates['created_at']).dt.days
+
+    print("missions_players_dates: \n",
+          missions_players_dates[missions_players_dates['diff_date_mission'] > 30].shape)
+
+    # ---------------------------------------------------------------------------------------
+
+    mission_access_merge = pd.merge(
+        missions_players_dates, app_accesses_dates, how='inner', on='id_player')
+
+    mission_access_merge['diff_date'] = abs(mission_access_merge['diff_date_mission'] -
+                                            mission_access_merge['diff_date_access'])
+
+    mission_access_merge['flag'] = mission_access_merge['diff_date'].apply(
+        lambda x: 0 if x > 30 else 1)
+
+    activeUsers = mission_access_merge
+
+    return activeUsers
+
+
+def award_details():
+    pass
 
 
 def main():
 
     registry_game, product_loaded, product_ean_conversion, missions_players, app_accesses, awards_moms = load_data()
-    check_missing_values(registry_game, product_loaded,
-                         product_ean_conversion, missions_players, app_accesses, awards_moms)
+    # check_missing_values(registry_game, product_loaded,
+    #                      product_ean_conversion, missions_players, app_accesses, awards_moms)
     # descriptive_stats(registry_game, product_loaded, product_ean_conversion, missions_players,app_accesses, awards_moms)
     # detect_outliers(registry_game)
     registry_game = deal_with_garbage_data(registry_game)
     # detect_outliers(registry_game)
 
-    # merge_df = merge_all_data(registry_game, product_loaded,
-    #                           product_ean_conversion, missions_players, app_accesses, awards_moms)
-    # feature_engineering(registry_game)
+    merge_df = tier_based_product(
+        product_loaded, product_ean_conversion, missions_players)
+    feature_engineering(registry_game)
     active_users(missions_players, app_accesses)
 
 
 main()
-
-# # Get today's date
-# todayDate = datetime.today().strftime('%Y-%m-%d')
-
-# # Convert today's date to datetime
-# todayDate = pd.DataFrame([pd.to_datetime(todayDate)], columns=['TodayDate'])
-
-# todayDate['TodayDate'] = pd.to_datetime(todayDate['TodayDate']).dt.date
-# print(todayDate)
